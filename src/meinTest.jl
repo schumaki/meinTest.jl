fcd5000b9f071159fd9312be815dd4eac5221078
Kristoph <kristoph.schumann@iqb.hu-berlin.de>
2022-07-06 16:25:10 +0200

    add documentation (kind of)

--- src/meinTest.jl
+++ src/meinTest.jl
@@ -1,5 +1,15 @@
 module meinTest
     include("LinReg.jl")
+    """
+    Predict new observations 
 
+    ## Arguments
+
+    - `x`: new observations
+    - `β`: optional weigths
+    ...
+
+    to be continued
+    """
     export sos
 end
